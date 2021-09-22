976c21c2e386ff26981c14417f47296f90fc0c85
Salomon <13030564@itcelaya.edu.mx>
2021-09-22 13:17:05 -0500

    Validaciones en FormularioTemplate, uso de ngModel y ngSubmit

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario = {
+    nombre:'Salomon',
+    apellido:'Canchola',
+    correo:'13030564@itcelaya.edu.mx'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar( forma: NgForm ){
+    console.log( forma );
+    console.log( forma.value );
   }
 
 }
