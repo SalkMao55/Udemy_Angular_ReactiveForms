54c484629f9f230330edf0c008222ccf5f0f6566
Salomon <13030564@itcelaya.edu.mx>
2021-09-23 13:01:22 -0500

    Alertas en Formulario Template

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -10,7 +10,7 @@ export class TemplateComponent implements OnInit {
 
   usuario = {
     nombre:'Salomon',
-    apellido:'Canchola',
+    apellido:'Canchola Espinoza',
     correo:'13030564@itcelaya.edu.mx'
   }
 
@@ -21,6 +21,18 @@ export class TemplateComponent implements OnInit {
 
   guardar( forma: NgForm ){
     console.log( forma );
+    
+    //Impedir guardar datos si no es VALIDO el FORMULARIO
+    if (forma.invalid) {
+      // The Object.values() method returns an array of a given object's own enumerable property values,
+      // in the same order as that provided by a for...in loop.
+      Object.values( forma.controls ).forEach( control => { //Por cada "Control" del formulario
+        //Marcar para mostrar alertas en Inputs
+        control.markAsTouched();
+      });
+      return;
+    }
+
     console.log( forma.value );
   }
 
