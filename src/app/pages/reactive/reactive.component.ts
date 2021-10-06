6b0adad7082a80dfd04289510b2fd306e47f6075
Salomon <13030564@itcelaya.edu.mx>
2021-10-06 16:43:16 -0500

    Reactivo: Creacion de formulario - FormGroup

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  // Por eso con el ! fuerzas la inicialización. (Strict Class Initialization)
+  forma!: FormGroup;
+
+  constructor( private fb:FormBuilder) { 
+    this.crearFormulario();
+   }
 
   ngOnInit(): void {
+  }
+
+  // Metodo para crear el formulario
+  crearFormulario(){
+    this.forma = this.fb.group({
+      nombre:  ['Salomon'],
+      apellido:['Canchola'],
+      correo:  ['13030564@itcelaya.edu.mx']
+    });
+  }
+
+  // Metodo para guardar el contenido del formulario
+  guardar(){
+    console.log(this.forma);
   }
 
 }
